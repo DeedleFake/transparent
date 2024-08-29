8fd9e07ed07c79fb98f6810fe8a8edb7f798bd75
DeedleFake <deedlefake@users.noreply.github.com>
2024-08-28 23:17:18 -0400

    transparent: implement `Clear()`

--- transparent.go
+++ transparent.go
@@ -1,5 +1,17 @@
 package transparent
 
-func Clear(url string) string {
-	return url
+import "deedles.dev/transparent/internal/rules"
+
+// Clear checks url against the known rules for cleaning a URL. If any
+// rules apply, the cleaned URL is returned. If no rules apply, the
+// originally passed URL and false are returned.
+//
+// If url is not a valid URL, the results are undefined.
+func Clear(url string) (string, bool) {
+	for _, provider := range rules.Providers {
+		if url, ok := provider.Clear(url); ok {
+			return url, true
+		}
+	}
+	return url, false
 }
