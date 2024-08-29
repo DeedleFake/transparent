f3698a1756804dd5dff750b30077d86e0dfc3de3
DeedleFake <deedlefake@users.noreply.github.com>
2024-08-28 23:20:00 -0400

    internal/rules: improve test a bit

--- internal/rules/rules_test.go
+++ internal/rules/rules_test.go
@@ -8,19 +8,21 @@ import (
 )
 
 var testProvider = rules.Provider{
-	URLPattern:       regexp.MustCompile(`^https?:\/\/(?:[a-z0-9-]+\.)*?9gag\.com`),
-	CompleteProvider: false,
-	Rules:            []*regexp.Regexp{regexp.MustCompile(`^ref$`)},
-	Exceptions:       []*regexp.Regexp{regexp.MustCompile(`^https?:\/\/comment-cdn\.9gag\.com\/.*?comment-list.json\?`)},
+	URLPattern:        regexp.MustCompile(`^https?:\/\/(?:[a-z0-9-]+\.)*?amazon(?:\.[a-z]{2,}){1,}`),
+	CompleteProvider:  false,
+	Rules:             []*regexp.Regexp{regexp.MustCompile(`^p[fd]_rd_[a-z]*$`), regexp.MustCompile(`^qid$`), regexp.MustCompile(`^srs?$`), regexp.MustCompile(`^__mk_[a-z]{1,3}_[a-z]{1,3}$`), regexp.MustCompile(`^spIA$`), regexp.MustCompile(`^ms3_c$`), regexp.MustCompile(`^[a-z%0-9]*ie$`), regexp.MustCompile(`^refRID$`), regexp.MustCompile(`^colii?d$`), regexp.MustCompile(`^[^a-z%0-9]adId$`), regexp.MustCompile(`^qualifier$`), regexp.MustCompile(`^_encoding$`), regexp.MustCompile(`^smid$`), regexp.MustCompile(`^field-lbr_brands_browse-bin$`), regexp.MustCompile(`^ref_?$`), regexp.MustCompile(`^th$`), regexp.MustCompile(`^sprefix$`), regexp.MustCompile(`^crid$`), regexp.MustCompile(`^keywords$`), regexp.MustCompile(`^cv_ct_[a-z]+$`), regexp.MustCompile(`^linkCode$`), regexp.MustCompile(`^creativeASIN$`), regexp.MustCompile(`^ascsubtag$`), regexp.MustCompile(`^aaxitk$`), regexp.MustCompile(`^hsa_cr_id$`), regexp.MustCompile(`^sb-ci-[a-z]+$`), regexp.MustCompile(`^rnid$`), regexp.MustCompile(`^dchild$`), regexp.MustCompile(`^camp$`), regexp.MustCompile(`^creative$`), regexp.MustCompile(`^s$`), regexp.MustCompile(`^content-id$`), regexp.MustCompile(`^dib$`), regexp.MustCompile(`^dib_tag$`)},
+	RawRules:          []*regexp.Regexp{regexp.MustCompile(`\/ref=[^/?]*`)},
+	ReferralMarketing: []*regexp.Regexp{regexp.MustCompile(`tag`), regexp.MustCompile(`ascsubtag`)},
+	Exceptions:        []*regexp.Regexp{regexp.MustCompile(`^https?:\/\/(?:[a-z0-9-]+\.)*?amazon(?:\.[a-z]{2,}){1,}\/gp\/.*?(?:redirector.html|cart\/ajax-update.html|video\/api\/)`), regexp.MustCompile(`^https?:\/\/(?:[a-z0-9-]+\.)*?amazon(?:\.[a-z]{2,}){1,}\/(?:hz\/reviews-render\/ajax\/|message-us\?|s\?)`)},
 }
 
 func TestClear(t *testing.T) {
-	const before = "https://9gag.com/something?ref=3&other=2"
+	const before = "https://amazon.com/something?ref=3&pf_rd_q=5&other=2"
 	after, ok := testProvider.Clear(before)
 	if !ok {
 		t.Fatalf("%q -> %q", before, after)
 	}
-	if after != "https://9gag.com/something?other=2" {
+	if after != "https://amazon.com/something?other=2" {
 		t.Fatalf("%q -> %q", before, after)
 	}
 }
